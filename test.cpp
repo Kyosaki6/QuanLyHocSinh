f7173f4b75ed5880ca44054eced937bd91b5ee24
huyhuy <23.1015.huydinhcong@gmail.com>
2025-11-10 14:44:41 +0700

    ap lai ham main

--- test.cpp
+++ test.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+using namespace std;
+
 
 bool soLe(int number){
     return (number % 2 == 1);
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << soLe(n);
+    return 0;
 }
